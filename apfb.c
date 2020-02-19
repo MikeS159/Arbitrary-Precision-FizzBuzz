b6ea6e368964c4d620c053d342445baadac2934e
Mike Simpson <mike.simpson@phabrix.com>
2020-02-19 09:14:22 +0000

    Varibles to hold fizz buzz numbers and fix for no input seg falt

--- apfb.c
+++ apfb.c
@@ -1,22 +1,25 @@
 #include <gmp.h>
 #include <stdio.h>
 int main(int argc, char *argv[]) {
-    if(argc > 0) {
+    int fizz = 3;
+    int buzz = 5;
+    int fizzBuzz = fizz * buzz;
+    if(argc > 1) {
         mpz_t inputNum;
         mpz_init(inputNum);
         mpz_set_str(inputNum, argv[1], 10);
         if(!mpz_cmp_si(inputNum, 0)) {
             printf("Not a integer!\n");
             return 0;
-        } if(mpz_divisible_ui_p(inputNum, 15)) {
+        } if(mpz_divisible_ui_p(inputNum, fizzBuzz)) {
             printf("FizzBuzz");
-        } else if(mpz_divisible_ui_p(inputNum, 5)) {
+        } else if(mpz_divisible_ui_p(inputNum, buzz)) {
             printf("Buzz");
-        } else if(mpz_divisible_ui_p(inputNum, 3)) {
+        } else if(mpz_divisible_ui_p(inputNum, fizz)) {
             printf("Fizz");
         } else {
             mpz_out_str(stdout, 10, inputNum);
-        }  
+        }
         printf("\n");
     }
 }
